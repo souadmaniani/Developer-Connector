6dc6ae2d1815eb711c02fd4b3c9a93d428551908
souadmaniani <soadmaniani98@gmail.com>
2021-10-17 18:58:41 +0000

    .

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -7,39 +7,47 @@ const ProfileGithub = ({ githubusername }) => {
     clientSecret: "bca84dac0132a4a635e99ee1a89eebaf94d9c1b6",
     count: 5,
     sort: "created:asc",
-    repos: [],
+    repos: {},
   });
   useEffect(() => {
     const { clientId, clientSecret, count, sort } = githubValues;
+    console.log(
+      process.env.REACT_APP_GITHUB_ENDPOINT +
+        `${githubusername}/repos?per_page=${count}&&sort=${sort}&&client_id=${clientId}&&client_secret=${clientSecret}`
+    );
     fetch(
       process.env.REACT_APP_GITHUB_ENDPOINT +
-        `/${githubusername}/repos?per_page=${count}&&sort=${sort}&&client_id=${clientId}&&client_secret=${clientSecret}`
+        `${githubusername}/repos?per_page=${count}&&sort=${sort}&&client_id=${clientId}&&client_secret=${clientSecret}`
     )
       .then((res) => res.json())
-      .then((data) => setgithubValues({ ...githubValues, repos: data }));
+      .then((data) => {
+        console.log(data);
+        setgithubValues({ ...githubValues, repos: data });
+      });
   }, []);
 
   const { repos } = githubValues;
-
-  const repoItems = repos.map((repo) => (
-    <div key={repo.id} className="card card-body mb-2">
+  const repoItems = Object.keys(repos).map((repoKey) => (
+    <div key={repos[repoKey].id} className="card card-body mb-2">
       <div className="row">
         <div className="col-md-6">
           <h4>
-            <Link to={repo.html_url} className="text-info" target="_blank">
-              {repo.name}
+            <Link to={repos[repoKey].html_url} className="text-info">
+              {repos[repoKey].name}
             </Link>
           </h4>
-          <p>{repo.description}</p>
+          <p>{repos[repoKey].description}</p>
         </div>
         <div className="col-md-6">
           <span className="badge badge-info mr-1">
-            Stars: {repo.stargazers_count}
+            Stars: {repos[repoKey].stargazers_count}
           </span>
           <span className="badge badge-secondary mr-1">
-            Watchers: {repo.watchers_count}
+            Watchers: {repos[repoKey].watchers_count}
           </span>
-          <span className="badge badge-success">Forks: {repo.forks_count}</span>
+          <span className="badge badge-success">
+            Forks: {repos[repoKey].forks_count}
+          </span>
         </div>
       </div>
     </div>
@@ -49,7 +57,7 @@ const ProfileGithub = ({ githubusername }) => {
     <div>
       <hr />
       <h3 className="mb-4">Latest Github Repos</h3>
-      {repos.length > 0 && repoItems}
+      {repoItems}
     </div>
   );
 };
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // mongoURI: "mongodb://localhost:27017/devConnector",
-    mongoURI: "mongodb+srv://souad:souadoussama98@cluster0.peubi.mongodb.net/DeveloperConnector?retryWrites=true&w=majority",
+    mongoURI: "mongodb://localhost:27017/devConnector",
+    // mongoURI: "mongodb+srv://souad:souadoussama98@cluster0.peubi.mongodb.net/DeveloperConnector?retryWrites=true&w=majority",
     secretOrKey: "ssothediamond"
 }
