73e913cb0ccb43b841bfbad9e27f363e5c84fbdd
Souad Maniani <smaniani@e2r13p12.1337.ma>
2021-10-19 07:59:18 +0100

    add keys_dev && keys_prod

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    // mongoURI: "mongodb://localhost:27017/devConnector",
-    mongoURI: "mongodb+srv://souad:souadoussama98@cluster0.peubi.mongodb.net/DeveloperConnector?retryWrites=true&w=majority",
-    secretOrKey: "ssothediamond"
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod')
+} else {
+    module.exports = require('./keys_dev')
 }
